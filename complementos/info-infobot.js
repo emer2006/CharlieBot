b536206e81c48ffab7c3d540791912d3b749ccaa
emer2006 <126901936+emer2006@users.noreply.github.com>
2023-03-07 17:57:14 -0500

    Update and rename plugins/info-infobot.js to complementos/info-infobot.js

--- complementos/info-infobot.js
+++ complementos/info-infobot.js
@@ -21,8 +21,8 @@ let speed = neww - old
 let info = `
 ╠═〘 𝐈𝐍𝐅𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 〙 ═
 ╠
-╠➥ [🤴🏻] 𝙲𝚁𝙴𝙰𝙳𝙾𝚁: *Ryu*
-╠➥ [#️⃣] 𝙽𝚄𝙼𝙴𝚁𝙾: *+51 955 095 498*
+╠➥ [🤴🏻] 𝙲𝚁𝙴𝙰𝙳𝙾𝚁: *CharlieBot*
+╠➥ [#️⃣] 𝙽𝚄𝙼𝙴𝚁𝙾: *+51 994 156 047*
 ╠➥ [🎳] 𝙿𝚁𝙴𝙵𝙸𝙹𝙾: *${usedPrefix}*
 ╠➥ [🔐] 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂: *${chats.length - groups.length}*
 ╠➥ [🦜] 𝙲𝙷𝙰𝚃𝚂 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂: *${groups.length}* 
@@ -40,7 +40,7 @@ let info = `
 ╠➥ [👨‍🦯] 𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳: 
 ╠  *${speed} ms* 
 ╠
-╠═〘 @ffxryu.18 〙 ═
+╠═〘 @emx._xit 〙 ═
 `.trim() 
 let aa = { quoted: m, userJid: conn.user.jid }
 let res = generateWAMessageFromContent (m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: info, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
